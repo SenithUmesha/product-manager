23d7c8c0df749c38a2f71df934b74611d9034ebf
Senith Umesha <90299964+SenithUmesha@users.noreply.github.com>
2026-09-17 15:26:12 +0530

    chore: add dashboard csrf metadata

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Product Manager</title>
 
     @include('libraries.styles')
 </head>
 
-<body>
-
+<body class="bg-light">
     @yield('content')
 
     @include('libraries.scripts')
