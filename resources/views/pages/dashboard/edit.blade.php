582cc1c16f34ab750c6084a3a2710e6e5692d0a4
Senith Umesha <90299964+SenithUmesha@users.noreply.github.com>
2026-09-17 15:26:05 +0530

    refactor: update products with PATCH

--- resources/views/pages/dashboard/edit.blade.php
+++ resources/views/pages/dashboard/edit.blade.php
@@ -1,13 +1,21 @@
-<form action="{{ route('dashboard.update', $product->id) }}" method="GET" enctype="multipart/form-data">
+<form action="{{ route('dashboard.update', $product->id) }}" method="POST">
     @csrf
-    <div class="mb-2">
-        <label for="productName" class="form-label">Name</label>
-        <input type="text" name="name" value="{{ $product->name }}" class="form-control" id="productName" required>
+    @method('PATCH')
+
+    <div class="mb-3">
+        <label for="editProductName" class="form-label">Name</label>
+        <input type="text" name="name" value="{{ $product->name }}" class="form-control" id="editProductName"
+            maxlength="255" required>
     </div>
+
     <div class="mb-4">
-        <label for="productPrice" class="form-label">Price</label>
-        <input type="number" name="price" value="{{ $product->price }}" min="0.00" max="10000.00" step="0.01"
-            class="form-control" id="productPrice" required>
+        <label for="editProductPrice" class="form-label">Price</label>
+        <input type="number" name="price" value="{{ $product->price }}" min="0" max="9999999.99" step="0.01"
+            class="form-control" id="editProductPrice" required>
     </div>
-    <button type="submit" class="btn btn-primary">Update</button>
+
+    <div class="d-flex justify-content-end gap-2">
+        <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancel</button>
+        <button type="submit" class="btn btn-primary">Save changes</button>
+    </div>
 </form>
