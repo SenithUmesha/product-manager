d7d1286dd98ecb08c46c47ead12403c03408e908
Senith Umesha <90299964+SenithUmesha@users.noreply.github.com>
2026-09-17 15:25:15 +0530

    refactor: use authenticated RESTful product routes

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,19 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::redirect('/', '/dashboard');
 
-Route::prefix('/dashboard') -> group(function () {
-    Route::get('/', [DashboardController::class, "index"]) -> name('dashboard');
-    Route::post('/store', [DashboardController::class, "store"]) -> name('dashboard.store');
-    Route::get('/{product_id}/delete', [DashboardController::class, "delete"]) -> name('dashboard.delete');
-    Route::get('/{product_id}/status', [DashboardController::class, "status"]) -> name('dashboard.status');
-    Route::get('/edit', [DashboardController::class, "edit"]) -> name('dashboard.edit');
-    Route::get('/{product_id}/update', [DashboardController::class, "update"]) -> name('dashboard.update');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->prefix('dashboard')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::post('/products', [DashboardController::class, 'store'])->name('dashboard.store');
+    Route::get('/products/edit', [DashboardController::class, 'edit'])->name('dashboard.edit');
+    Route::patch('/products/{product_id}', [DashboardController::class, 'update'])->name('dashboard.update');
+    Route::patch('/products/{product_id}/status', [DashboardController::class, 'status'])->name('dashboard.status');
+    Route::delete('/products/{product_id}', [DashboardController::class, 'delete'])->name('dashboard.delete');
 });
